dadc4e402f362bc5fb50bd8873c5798f73d4f99a
jshwnth <jashusatish@gmail.com>
2019-03-02 14:48:18 +0530

    final

--- test.py
+++ test.py
@@ -9,3 +9,17 @@ def mul(x,y):
 
 def div(x,y)
     return x//y
+
+a,b=int(input().split())
+
+print('1.add/n 2.sub/n 3.mul/n 4.div /n')
+c=int(input())
+
+if c==1:
+    print(add(a,b))
+elif c==2:
+    print(sub(a,b))
+elif c == 3:
+    print(mul(a, b))
+elif c==4:
+    print(div(a,b))
